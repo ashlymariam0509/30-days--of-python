28eb2bbd3b1f8b4a39cb9e35233946d3ed429abd
ashlymariam0509 <ashlysank2006@gmail.com>
2024-11-20 23:06:28 +0530

    Add files via upload

---
+++ Day-1/day1 Ashlytask3operators.py
@@ -0,0 +1,8 @@
+num1=int(input('enter a number:'))
+num2=int(input('enter a number:'))
+sum=num1+num2
+print('sum=',sum)
+difference=num1-num2
+print('difference=',difference)
+product=num1*num2
+print('product=',product)
