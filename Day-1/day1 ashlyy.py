cb5710fd8c3f6d924d7387340c0152491612677b
ashlymariam0509 <ashlysank2006@gmail.com>
2024-11-20 21:23:19 +0530

    Add files via upload

---
+++ Day-1/day1 ashlyy.py
@@ -0,0 +1,16 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("ashly")
+ashly
+>>> print(18)
+18
+>>> 
+========================================================= RESTART: C:/Users/agabr/day1 ashly.py =========================================================
+ashly
+18
+Indian
+>>> 
+========================================================= RESTART: C:/Users/agabr/day1 ashly.py =========================================================
+ashly
+18
+Indian
