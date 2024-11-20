f4da951bdbc403b0b592be2313567415fa3f75eb
snehaannjk <snehaannajacob1968@gmail.com>
2024-11-20 21:53:38 +0530

    Add files via upload

---
+++ Day-1/arithemetic operators.py
@@ -0,0 +1,8 @@
+number1=int(input('enter a number:'))
+number2=int(input('enter a number:'))
+sum=number1+number2
+print('sum=',sum)
+difference=number1-number2
+print('difference=',difference)
+product=number1*number2
+print('product=',product)
