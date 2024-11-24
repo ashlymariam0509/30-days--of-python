ba9e60f538b73cdb638c1cf573dc0f34f113a995
SamithaSabu <samithasabu4857@gmail.com>
2024-11-24 14:03:03 +0530

    Create Sami_trapezoid.py

---
+++ DAY 3/Sami_trapezoid.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the first base of trapezoid:"))
+b=int(input("Enter the second base of the trapezoid:"))
+h=int(input("Enter the height of the trapezoid:"))
+area=1/2*(a+b)*h
+print("Area of the trapezoid of bases ",a,"and",b,"and height",h,"is",area )
