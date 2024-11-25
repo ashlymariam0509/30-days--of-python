cbf59a2d670bb687a6676405ce6bd34e03ff61c6
Stivance K Baby <144829815+Stivance04@users.noreply.github.com>
2024-11-26 01:09:32 +0530

    Add files via upload

---
+++ DAY 3/Stiv day3/Q1.py
@@ -0,0 +1,3 @@
+import math
+x=int(input('enter the number to find the square root'))
+print(math.sqrt(x))
---
+++ DAY 3/Stiv day3/Q2.py
@@ -0,0 +1,5 @@
+a=int(input('enter the first base length '))
+b=int(input('enter the second base length '))
+h=int(input('enter the hight '))
+area=((a+b)/2)*h
+print('area= ',area)
