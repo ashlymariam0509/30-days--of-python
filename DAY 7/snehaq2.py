27e50258b519e2f0131745951609d06cc4d2910b
snehaannjk <snehaannajacob1968@gmail.com>
2024-12-01 20:37:25 +0530

    Add files via upload

---
+++ DAY 7/snehaq2.py
@@ -0,0 +1,6 @@
+sum = 0
+n = int(input("Enter number :"))
+for i in range(1, n + 1, 1):
+    sum += i
+print("\n")
+print("Sum is: ", sum)
---
+++ DAY 7/snehaq4.py
@@ -0,0 +1,4 @@
+list1 = [1,2,3,4,5,6]
+new_list = reversed(list1)
+for item in new_list:
+    print(item)
