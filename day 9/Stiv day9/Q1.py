6146c51a8c5055e98a8a0f538c92c431175b6430
Stivance K Baby <144829815+Stivance04@users.noreply.github.com>
2024-12-02 23:33:17 +0530

    Add files via upload

---
+++ day 9/Stiv day9/Q1.py
@@ -0,0 +1,7 @@
+n=int(input("enter how many numbers you wamt to input"))
+a=0
+for i in range(n):
+    b=int(input('enter the number'))
+    a=a+b
+print(a)    
+print(a/n,'is the average')
---
+++ day 9/Stiv day9/Q2.py
@@ -0,0 +1,7 @@
+l=[]
+c=1
+while len(l)<10:
+    c+=1
+    if c%2==0:
+        l.append(c)
+print(l)
