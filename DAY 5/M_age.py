b3ef1aeb7a60a74068ddc42cc3472c84c0fb0235
Milton Michael <miltonmichaelmattappallil@gmail.com>
2024-12-03 21:14:41 +0530

    Add files via upload

---
+++ DAY 5/M_age.py
@@ -0,0 +1,5 @@
+age=int(input("Enter the age of the person:"))
+if age >= 18:
+  print("The person of age",age," is eligible to vote")
+else:
+  print("The person of age",age,"is not eligible to vote")
