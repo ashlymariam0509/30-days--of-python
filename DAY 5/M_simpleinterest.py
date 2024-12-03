ae4831b0047e0092a455d9a7e5ec00ed89600aa7
Milton Michael <miltonmichaelmattappallil@gmail.com>
2024-12-03 21:17:17 +0530

    Add files via upload

---
+++ DAY 5/M_simpleinterest.py
@@ -0,0 +1,5 @@
+P=int(input("Enter the principle amount :"))
+R=int(input("Enter the rate of interest :"))
+T=int(input("Enter the time period :"))
+simpleinterest=(P*R*T)/100
+print("The simple interest value is",simpleinterest)
