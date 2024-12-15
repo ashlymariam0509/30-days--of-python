890cdc9d8f0449497f47e4c5cc55c2a19fdcaa88
Milton Michael <miltonmichaelmattappallil@gmail.com>
2024-12-15 20:27:25 +0530

    Add files via upload

---
+++ DAY 14/M_dictionary.py
@@ -0,0 +1,6 @@
+d = {"Name":"Diya" , "Age":23}
+print(d)
+d.update({"City":"kollam"})
+print(d)
+d["Gender"]="Female"
+print(d)
---
+++ DAY 14/M_index.py
@@ -0,0 +1,6 @@
+student = {"Name": "Diya", "Age":25, "Gender": "Female", "City": "kolkata", "Mark":360}
+print(list(student)[0])
+print(list(student)[1])
+print(list(student)[2])
+print(list(student)[3])
+print(list(student)[4])
