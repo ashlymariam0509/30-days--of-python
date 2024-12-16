7539975b915a7b377eaa77913092cbb47830c92a
ashlymariam0509 <ashlysank2006@gmail.com>
2024-12-16 18:05:20 +0530

    Add files via upload

---
+++ DAY 15/DAY 15 Ashpalindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(str):
+    if len(str)<= 1:
+        return True
+    elif str[0] == str[-1]:
+        return is_palindrome(str[1:-1])
+    else:
+        return False
+str=input("Enter the string:")
+print(is_palindrome(str))
